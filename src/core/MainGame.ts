508c41dcbd49160e0978caf6ee1af4177f505a1a
Hung <Emailnguyenthanhhung@gmail.com>
2024-04-01 08:35:37 +0700

    update UI image stage game

--- src/core/MainGame.ts
+++ src/core/MainGame.ts
@@ -1,6 +1,6 @@
 import * as PIXI from 'pixi.js';
 import { Tetromino, IShape, LShape, JShape, OShape, TShape, SShape, ZShape } from './tetromino';
-class Game {
+export class Game {
     score: number;
     boardWidth: number;
     boardHeight: number;
@@ -19,10 +19,9 @@ class Game {
         this.currentBoard = Array.from({ length: this.boardHeight }, () => Array(this.boardWidth).fill(0));
         this.landedBoard = Array.from({ length: this.boardHeight }, () => Array(this.boardWidth).fill(0));
         this.currentTetromino = this.randomTetromino();
-
-        this.app = new PIXI.Application({ width: 500, height: 650, backgroundColor: 0xFFFFFF });
+        this.app = new PIXI.Application({ width: 560, height: 600, backgroundColor: 0xFFFFFF });
         document.body.appendChild(this.app.view);
-        this.blockSize = 24;
+        this.blockSize = 25;
         this.padding = 4;
         this.blockColors = [
             0xFF0000, // Đỏ
@@ -37,14 +36,11 @@ class Game {
             0x00FF99, // Màu xanh lá cây nhạt
         ];
     }
-
     draw() {
         const graphics = new PIXI.Graphics();
         this.app.stage.addChild(graphics);
-
         graphics.clear();
         graphics.lineStyle(1, 0x800080);
-
         // Vẽ khung board
         graphics.drawRect(this.padding, this.padding, this.blockSize * this.boardWidth + this.padding * (this.boardWidth + 1), this.blockSize * (this.boardHeight - 3) + this.padding * (this.boardHeight - 3 + 1));
 
@@ -57,35 +53,39 @@ class Game {
                 graphics.endFill();
             }
         }
-
-        const gameTitle = PIXI.Sprite.from('../assets/logo_tetris.png'); 
-        gameTitle.position.set(310, 10);
-        gameTitle.width = 150; 
-        gameTitle.height = 25; 
+        const gameTitle = PIXI.Sprite.from('../assets/logo_tetris.png');
+        gameTitle.position.set(325, 10);
+        gameTitle.width = 200;
+        gameTitle.height = 50;
         this.app.stage.addChild(gameTitle);
-
-        const nextText = new PIXI.Text('Continue:', { fontSize: '25px', fill: 0x000000 });
-        nextText.position.set(300, 60);
+        const nextText = new PIXI.Text('Next:', { fontSize: '28px', fill: '#87CEEB' });
+        nextText.position.set(310, 100);
         this.app.stage.addChild(nextText);
 
-        const scoreText = new PIXI.Text('Scores:', { fontSize: '25px', fill: 0x000000 });
-        scoreText.position.set(300, 300);
+        const scoreText = new PIXI.Text('Scores:', { fontSize: '25px', fill: '#87CEEB' });
+        scoreText.position.set(310, 300);
         this.app.stage.addChild(scoreText);
 
+        const Level = new PIXI.Text('Level:', { fontSize: '25px', fill: '#87CEEB' });
+        Level.position.set(310, 330);
+        this.app.stage.addChild(Level);
 
-        const scoreValue = new PIXI.Text(this.score.toString(), { fontSize: '28px', fill: 0x000000 });
-        scoreValue.position.set(400, 300);
+        const scoreValue = new PIXI.Text(this.score.toString(), { fontSize: '28px', fill: '#87CEEB' });
+        scoreValue.position.set(410, 300);
         this.app.stage.addChild(scoreValue);
 
-
-        const stage = PIXI.Sprite.from('../assets/button_reset_up.png'); 
-        stage.position.set(300, 500);
-        stage.width = 175; 
-        stage.height = 50; 
+        const stage = PIXI.Sprite.from('../assets/button_reset_up.png');
+        stage.position.set(340, 525);
+        stage.width = 175;
+        stage.height = 50;
         this.app.stage.addChild(stage);
+        stage.interactive = true;
+        stage.buttonMode = true;
+        stage.on('click', () => {
+            location.reload();
+        });
 
     }
-
     randomTetromino(): Tetromino {
         const randNum = Math.floor(Math.random() * 7);
         switch (randNum) {
@@ -109,18 +109,15 @@ class Game {
     }
     canTetrominoMoveSideways(direction: string): boolean {
         const offset = direction === 'left' ? -1 : 1;
-
         for (let i = 0; i < this.currentTetromino.height; i++) {
             for (let j = 0; j < this.currentTetromino.width; j++) {
                 if (this.currentTetromino.shape[i][j] > 0) {
                     const row = this.currentTetromino.row + i;
                     const col = this.currentTetromino.col + j + offset;
-
                     // Kiểm tra xem có di chuyển ra ngoài biên trái/phải không
                     if (col < 0 || col >= this.boardWidth) {
                         return false;
                     }
-
                     // Kiểm tra xem có đè lên các khối hộp đã ổn định không
                     if (row >= 0 && row < this.boardHeight && this.landedBoard[row][col] > 0) {
                         return false;
@@ -168,7 +165,6 @@ class Game {
         }
     }
 
-
     progress() {
         if (this.canTetrominoFall()) {
             this.currentTetromino.fall();
@@ -176,13 +172,43 @@ class Game {
             this.updateLandedBoard();
             this.currentTetromino = this.randomTetromino();
             if (!this.canTetrominoFall()) {
-                console.log("Game Over!");
+                // console.log("Game Over!");
+                this.showGameOverDialog();
                 return;
             }
         }
     }
 
+    showGameOverDialog() {
+        // Tạo hộp thoại Game Over
+        const dialog = new PIXI.Graphics();
+        dialog.beginFill(0x000000, 0.5);
+        dialog.drawRect(0, 0, this.app.renderer.width, this.app.renderer.height);
+        dialog.endFill();
+        dialog.zIndex = 9999;
+        this.app.stage.addChild(dialog);
 
+        // Thêm văn bản "Game Over!"
+        const gameOverText = new PIXI.Text('Game Over!', { fontFamily: 'Arial', fontSize: 43, fill: 0xFF0000, fontWeight: 'bold' });
+        gameOverText.anchor.set(0.5);
+        gameOverText.position.set(430, 400);
+        dialog.addChild(gameOverText);
+
+
+        const reloadButton = PIXI.Sprite.from('../assets/R.png');
+        reloadButton.width = 200;
+        reloadButton.height = 50;
+        reloadButton.interactive = true;
+        reloadButton.buttonMode = true;
+        reloadButton.position.set(330, 430);
+        dialog.addChild(reloadButton);
+
+        // Xử lý sự kiện khi nhấn nút "Reload Game"
+        reloadButton.on('pointerdown', () => {
+            location.reload();
+        });
+
+    }
     canTetrominoFall(): boolean {
         for (let i = 0; i < this.currentTetromino.height; i++) {
             for (let j = 0; j < this.currentTetromino.width; j++) {
